f52dbb31ec92af17242a9486e94907205187421a
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-25 03:45:23 +0100

    change blog routers to db

--- src/routers/blogs-router.ts
+++ src/routers/blogs-router.ts
@@ -1,5 +1,5 @@
 import {Router, Request, Response} from "express";
-import {BlogsInMemoryRepository} from "../repositories/blogs-in-memory-repository";
+import {BlogsInMemoryRepository} from "../repositories/blogs-db-repository";
 import {Params, RequestWithBody, RequestWithBodyAndParams, RequestWithParams} from "../types/common";
 import {CreateBlogDto, UpdateBlogDto} from "../types/blogs/input";
 import {inputValidationMiddleware} from "../middlewares/validators/input-validation-middleware";
@@ -10,13 +10,13 @@ import {HTTP_STATUSES} from "../utils/comon";
 
 export const blogsRouter = Router();
 
-blogsRouter.get("/", (req: Request, res: Response) => {
-    const blogs = BlogsInMemoryRepository.getAllBlogs();
+blogsRouter.get("/", async (req: Request, res: Response) => {
+    const blogs = await BlogsInMemoryRepository.getAllBlogs();
     res.status(HTTP_STATUSES.OK_200).json(blogs);
 })
 
-blogsRouter.get("/:id" , (req: RequestWithParams<Params>, res: Response) => {
-    const blog = BlogsInMemoryRepository.getBlogById(req.params.id);
+blogsRouter.get ("/:id", async (req: RequestWithParams<Params>, res: Response) => {
+    const blog = await BlogsInMemoryRepository.getBlogById(req.params.id);
     if (blog) {
         res.status(HTTP_STATUSES.OK_200).json(blog);
         return
@@ -25,10 +25,10 @@ blogsRouter.get("/:id" , (req: RequestWithParams<Params>, res: Response) => {
     }
 })
 
-blogsRouter.post('/',basicAuthorizationMiddleware,validationBlogsChains(),inputValidationMiddleware, (req: RequestWithBody<CreateBlogDto>, res: Response) => {
+blogsRouter.post('/', basicAuthorizationMiddleware, validationBlogsChains(), inputValidationMiddleware, async (req: RequestWithBody<CreateBlogDto>, res: Response) => {
     const creatData = req.body;
-    const blogID = BlogsInMemoryRepository.createBlog(creatData);
-    const newBlog = BlogsInMemoryRepository.getBlogById(blogID);
+    const blogID = await BlogsInMemoryRepository.createBlog(creatData);
+    const newBlog = await BlogsInMemoryRepository.getBlogById(blogID);
     if (newBlog) {
         res.status(HTTP_STATUSES.CREATED_201).json(newBlog);
         return
@@ -36,9 +36,9 @@ blogsRouter.post('/',basicAuthorizationMiddleware,validationBlogsChains(),inputV
     res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
 })
 
-blogsRouter.put("/:id",basicAuthorizationMiddleware,validationBlogsChains(),inputValidationMiddleware, (req:RequestWithBodyAndParams<Params, UpdateBlogDto>, res:Response)=>{
+blogsRouter.put("/:id", basicAuthorizationMiddleware, validationBlogsChains(), inputValidationMiddleware, async (req: RequestWithBodyAndParams<Params, UpdateBlogDto>, res: Response) => {
     const updateData = req.body;
-    const isUpdated = BlogsInMemoryRepository.updateBlog(req.params.id, updateData);
+    const isUpdated = await BlogsInMemoryRepository.updateBlog(req.params.id, updateData);
     if (isUpdated) {
         res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
         return
@@ -46,11 +46,11 @@ blogsRouter.put("/:id",basicAuthorizationMiddleware,validationBlogsChains(),inpu
     res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
 })
 
-blogsRouter.delete("/:id",basicAuthorizationMiddleware, (req:RequestWithParams<Params>, res:Response)=>{
-    const isDeleted = BlogsInMemoryRepository.deleteBlog(req.params.id);
-    if (isDeleted){
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-    return;
+blogsRouter.delete("/:id", basicAuthorizationMiddleware,async (req: RequestWithParams<Params>, res: Response) => {
+    const isDeleted = await BlogsInMemoryRepository.deleteBlog(req.params.id);
+    if (isDeleted) {
+        res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+        return;
     }
     res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
 })
