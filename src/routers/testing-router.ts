0fe959319fdbc4b71ecaa4df110d49ce462f8d8b
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-25 23:34:55 +0100

    fix error with deleting all data

--- src/routers/testing-router.ts
+++ src/routers/testing-router.ts
@@ -1,11 +1,11 @@
-import {Router,Request, Response} from "express";
+import {Router, Request, Response} from "express";
 
 import {blogCollection, dbBlogs, postCollection, videosCollection} from "../db/db-collections";
 
 export const testingRouter = Router()
 
-testingRouter.delete("/all-data", async (req:Request, res:Response)=>{
-
+testingRouter.delete("/all-data", async (req: Request, res: Response) => {
+    await blogCollection.deleteMany({});
     await postCollection.deleteMany({});
     await videosCollection.deleteMany({});
 
