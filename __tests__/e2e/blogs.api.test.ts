e03158c918f69634a2398dffaa5102d46142eb04
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-28 01:48:36 +0100

    switch on @before all@

--- __tests__/e2e/blogs.api.test.ts
+++ __tests__/e2e/blogs.api.test.ts
@@ -8,15 +8,14 @@ const routerName = "/blogs/";
 
 describe(routerName, () => {
     // clear DB before testing
-  /*  beforeAll(async () => {
-       //await request(app).delete("/testing/all-data");
+ beforeAll(async () => {
+       await request(app).delete("/testing/all-data");
     })
 
     it("01 - should be return 200 and empty array", async () => {
         await request(app).get(routerName).expect(200, []);
     })
 
-*/
     // POST requests
 
     it("02 - POST does not create new blog with incorrect data (empty fields)", async () => {
