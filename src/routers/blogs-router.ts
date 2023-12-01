462ab8fea84ea68a51536fa4b83875334dd5be7c
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-02 00:48:19 +0100

    add function create post for specific blog

--- src/routers/blogs-router.ts
+++ src/routers/blogs-router.ts
@@ -5,7 +5,7 @@ import {
     RequestWithBody,
     RequestWithBodyAndParams,
     RequestWithParams,
-    RequestWithSearchTerms, SortDataRepositoryType, QueryDataRequestType, SearchDataRepositoryType
+    RequestWithSearchTerms, SortBlogRepositoryType, QueryBlogRequestType, SearchBlogRepositoryType
 } from "../types/common";
 import {CreateBlogDto, UpdateBlogDto} from "../types/blogs/input";
 import {inputValidationMiddleware} from "../middlewares/validators/input-validation-middleware";
@@ -13,25 +13,27 @@ import {validationBlogsChains} from "../middlewares/validators/blogs-validators"
 import {basicAuthorizationMiddleware} from "../middlewares/auth/auth-middleware";
 import {HTTP_STATUSES} from "../utils/comon";
 import {BlogsQueryRepository} from "../repositories/blogs-query-repository";
+import {validationPostsChains, validationPostsChainsNoBlogId} from "../middlewares/validators/posts-validators";
+import {CreatePostDto} from "../types/posts/input";
 
 
 export const blogsRouter = Router();
 
-blogsRouter.get("/", async (req: RequestWithSearchTerms<QueryDataRequestType>, res: Response) => {
+blogsRouter.get("/", async (req: RequestWithSearchTerms<QueryBlogRequestType>, res: Response) => {
 
-    const query:QueryDataRequestType = req.query
+    const query: QueryBlogRequestType = req.query
 
-    const sortData: SortDataRepositoryType = {
+    const sortData: SortBlogRepositoryType = {
         sortBy: query.sortBy || "createdAt",
         sortDirection: query.sortDirection || "desc",
         pageNumber: query.pageNumber || 1,
         pageSize: query.pageSize || 10
     }
-    const searchData: SearchDataRepositoryType = {
+    const searchData: SearchBlogRepositoryType = {
         searchNameTerm: query.searchNameTerm || null
     }
 
-    const blogs = await BlogsQueryRepository.getAllBlogs(sortData,searchData);
+    const blogs = await BlogsQueryRepository.getAllBlogs(sortData, searchData);
     res.status(HTTP_STATUSES.OK_200).json(blogs);
 })
 
@@ -45,29 +47,54 @@ blogsRouter.get("/:id", async (req: RequestWithParams<Params>, res: Response) =>
     }
 })
 
-blogsRouter.post('/', basicAuthorizationMiddleware, validationBlogsChains(), inputValidationMiddleware, async (req: RequestWithBody<CreateBlogDto>, res: Response) => {
-    const creatData = req.body;
-    const blogID = await BlogsRepository.createBlog(creatData);
-    const newBlog = await BlogsRepository.getBlogById(blogID);
-    if (newBlog) {
-        res.status(HTTP_STATUSES.CREATED_201).json(newBlog);
-        return
-    }
-    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-})
+blogsRouter.post('/',
+    basicAuthorizationMiddleware,
+    validationBlogsChains(),
+    inputValidationMiddleware,
+    async (req: RequestWithBody<CreateBlogDto>, res: Response) => {
+        const creatData = req.body;
+        const blogID = await BlogsRepository.createBlog(creatData);
+        const newBlog = await BlogsRepository.getBlogById(blogID);
+        if (newBlog) {
+            res.status(HTTP_STATUSES.CREATED_201).json(newBlog);
+            return
+        }
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+    })
 
-blogsRouter.put("/:id", basicAuthorizationMiddleware, validationBlogsChains(), inputValidationMiddleware, async (req: RequestWithBodyAndParams<Params, UpdateBlogDto>, res: Response) => {
-    const updateData = req.body;
-    const isUpdated = await BlogsRepository.updateBlog(req.params.id, updateData);
-    if (isUpdated) {
-        res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-        return
+blogsRouter.post("/:id/post"),
+    basicAuthorizationMiddleware,
+    validationPostsChainsNoBlogId(),
+    inputValidationMiddleware,
+    async (req: RequestWithBodyAndParams<Params, CreatePostDto>, res: Response) => {
+        const blogId = req.params.id;
+        const createData = req.body;
+        const createdPost = await BlogsRepository.createPostToBlog(blogId,createData);
+        if (!createdPost) {
+            res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+            return
+        }
+        res.status(HTTP_STATUSES.CREATED_201).json(createdPost)
     }
-    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
-})
 
-blogsRouter.delete("/:id", basicAuthorizationMiddleware, async (req: RequestWithParams<Params>, res: Response) => {
-    const isDeleted = await BlogsRepository.deleteBlog(req.params.id);
-    if (isDeleted) res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
-    else res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
-})
+blogsRouter.put("/:id",
+    basicAuthorizationMiddleware,
+    validationBlogsChains(),
+    inputValidationMiddleware,
+    async (req: RequestWithBodyAndParams<Params, UpdateBlogDto>, res: Response) => {
+        const updateData = req.body;
+        const isUpdated = await BlogsRepository.updateBlog(req.params.id, updateData);
+        if (isUpdated) {
+            res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+            return
+        }
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+    })
+
+blogsRouter.delete("/:id",
+    basicAuthorizationMiddleware,
+    async (req: RequestWithParams<Params>, res: Response) => {
+        const isDeleted = await BlogsRepository.deleteBlog(req.params.id);
+        if (isDeleted) res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
+        else res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+    })
