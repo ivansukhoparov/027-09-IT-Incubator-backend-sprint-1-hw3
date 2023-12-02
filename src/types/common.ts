686ba6f661644516bc5543ae73548c5b9e06ae80
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-02 01:40:33 +0100

    add RequestWithSearchTermsAndParams

--- src/types/common.ts
+++ src/types/common.ts
@@ -4,6 +4,7 @@ export type RequestWithParams<P> = Request<P,{},{},{}>
 export type RequestWithBody<B> = Request<{},{},B,{}>
 export type RequestWithBodyAndParams<P,B> =Request<P, {}, B, {}>
 export type RequestWithSearchTerms<T> =Request<{}, {}, {}, T>
+export type RequestWithSearchTermsAndParams<P,T> =Request<P, {}, {}, T>
 
 export type Params ={
     id: string
