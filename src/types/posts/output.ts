54e2cf5e3dab20aca1f033dd0dda9c544fd5b755
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-02 01:41:55 +0100

    rename OutputPostType to PostOutputType add PostsViewModelType

--- src/types/posts/output.ts
+++ src/types/posts/output.ts
@@ -1,19 +1,27 @@
-export type OutputPostType ={
+export type PostsViewModelType = {
+    pagesCount: number
+    page: number
+    pageSize: number
+    totalCount: number
+    items: PostOutputType[]
+}
+
+export type PostOutputType = {
     id: string
     title: string
     shortDescription: string
     content: string
     blogId: string
     blogName: string
-    createdAt:string
+    createdAt: string
 }
 
-export type PostType ={
+export type PostType = {
     title: string
     shortDescription: string
     content: string
     blogId: string
     blogName: string
-    createdAt:string
+    createdAt: string
 }
 
